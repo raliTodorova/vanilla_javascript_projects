8c21e0877a3e3ad953441cb3ec658628db12799f
Ralitsa Todorova <r.v.todorova94@gmail.com>
2021-04-18 13:23:56 +0300

    completed html and css file

---
+++ app.js
@@ -0,0 +1,108 @@
+const products = [
+  {
+    name:'Ball Pein Hammer',
+    category:'hammer',
+    price:5,
+    image:'./images/ball_pein_hammer.jpg'
+  },
+  {
+    name:'Broad Axe',
+    category:'axe',
+    price:5,
+    image:'./images/broad_axe.jpg'
+  },
+  {
+    name:'Claw Hammer',
+    category:'hammer',
+    price:5,
+    image:'./images/claw_hammer.jpg'
+  },
+  {
+    name:'Club Hammer',
+    category:'hammer',
+    price:5,
+    image:'./images/club_hamer.jpg'
+  },
+  {
+    name:'Combination Wrench',
+    category:'wrench',
+    price:5,
+    image:'combination_wrench.jpg'
+  },
+  {
+    name:'Flare Nut Wrench',
+    category:'wrench',
+    price:5,
+    image:'flare_nut_wrench.jpg'
+  },
+  {
+    name:'Flat Screwdriver',
+    category:'screwdriver',
+    price:5,
+    image:'./images/flat_screwdriver.jpg'
+  },
+  {
+    name:'Flex Head Socket Wrench',
+    category:'wrench',
+    price:5,
+    image:'./images/flex_head_socket_wrench.jpg'
+  },
+  {
+    name:'Hand Saw',
+    category:'saw',
+    price:5,
+    image:'./images/hand_saw.jpg'
+  },
+  {
+    name:'Hatchet Axe',
+    category:'axe',
+    price:5,
+    image:'./images/hatchet_axe.jpg'
+  },
+  {
+    name:'Hex Key Screwdriver',
+    category:'screwdriver',
+    price:8.05,
+    image:'hex_key_screwdriver.jpg'
+  },
+  {
+    name:'Hudson Bay Axe',
+    category:'axe',
+    price:15.65,
+    image:'./images/hudson_bay_axe.jpg'
+  },
+  {
+    name:'Japanese Saw',
+    category:'saw',
+    price:10,
+    image:'./images/japanese_saw.jpg'
+  },
+  {
+    name:'Phillips Screwdriver',
+    category:'screwdriver',
+    price:5.99,
+    image:'./images/phillips_screwdriver.jpg'
+  },
+  {
+    name:'Ratcheting Box Wrench',
+    category:'wrench',
+    price:10.05,
+    image:'./images/ratcheting_box_wrench.jpg'
+  },
+  {
+    name:'Sledge Hammer',
+    category:'hammer',
+    price:25.99,
+    image:'./images/sledge_hammer.jpg'
+  },
+  {
+    name:'Wrench',
+    category:'wrench',
+    price:10.99,
+    image:'./images/wrench.jpg'
+  }
+]
+
+const filters = document.querySelector('.filters')
+const searchBar = document.querySelector('.search-bar input')
+const searchBtn = document.querySelector('.search')
